5ef846157aad577a9f33b76143be8d4126d29440
Jonghoon Yoon <by.newy@gmail.com>
2024-12-24 21:07:48 +0900

    #1 feat: 찾을 수 없는 URL을 입력한 경우 Home 으로 이동

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import {Route, Routes} from "react-router-dom";
+import {Navigate, Route, Routes} from "react-router-dom";
 import Home from "./Home.tsx";
 import About from "./About.tsx";
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home/>} index={true}/>
+      <Route path="/" element={<Home/>}/>
       <Route path="/about" element={<About/>}/>
+      <Route path="*" element={<Navigate to="/" replace/>}/> // 찾을 수 없는 URL인 경우 Home 으로 이동
     </Routes>
   )
 }

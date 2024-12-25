2f4b76cf957488eb918d8aa40eee6fa2696445f9
Jonghoon Yoon <by.newy@gmail.com>
2024-12-25 20:03:10 +0900

    #5 feat: 단위 테스트 환경 구성

---
+++ __tests__/folder1/sample1.ts
@@ -0,0 +1,5 @@
+import {assert, test} from "vitest";
+
+test("simple", () => {
+  assert.equal(2, 2)
+})
---
+++ __tests__/sample.ts
@@ -0,0 +1,7 @@
+import {assert, describe, it} from "vitest";
+
+describe("simple", () => {
+  it("a", () => {
+    assert.equal(0, 0);
+  })
+})
